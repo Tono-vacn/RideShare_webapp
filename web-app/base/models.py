896a6a52de88767d1a3a7b68ecfb3325ab25b0b4
Tono-vacn <yuchen.jiang@duke.edu>
2024-01-21 23:56:31 -0500

    add model

--- web-app/base/models.py
+++ web-app/base/models.py
@@ -1,3 +1,4 @@
+import email
 from email.policy import default
 from random import choices
 from unittest.util import _MAX_LENGTH
@@ -25,6 +26,22 @@ class LoginUserManager(BaseUserManager):
     new_user.set_password(psw)
     new_user.save(using = self._db)
     return new_user
+  
+  def create_super_user(self, email_input, psw, first_name = "NULL", last_name = "NULL", phone_num = "+19841234567", user_group = "PASSENGER", driver_license = "", plate_num = ""):
+    sp_user = self.model(
+      email = LoginUserManager.normalize_email(email_input),
+      last_name = last_name,
+      first_name = first_name,
+      user_group = user_group,
+      phone_num = phone_num,
+    )
+    
+    sp_user.is_admin = True
+    sp_user.is_staff = True
+    sp_user.is_superuser = True
+    sp_user.is_active = True
+    sp_user.save(using = self._db)
+    return sp_user
 
 class User(AbstractUser):
   first_name = models.CharField(max_length = 64, default = "NULL", help_text = "First Name")
@@ -40,11 +57,23 @@ class User(AbstractUser):
   user_group = models.CharField(max_length = 10, choices = USER_GROUP, default = "Passenger")
   license_num = models.CharField(max_length = 64, null = True, blank = True, help_text = "License Number")
   plate_num = models.CharField(max_length = 20, null = True, blank = True, help_text = "Plate Number")
+  max_passenger = models.IntegerField(null = True, blank = True, help_text = "Max Passenger")
   
+  VEHICLE_TYPE = (
+      ('Economy', '0'),
+      ('Comfort', '1'),
+      ('Large', '2'),
+      ('XL', '3')
+  )
+  
+  vehicle_type = models.CharField(max_length = 20, null=True, blank=True, choices = VEHICLE_TYPE, default = "Economy")
+  vehicle_brand = models.CharField(max_length = 20, null=True, blank=True)
   
   objects = LoginUserManager()
   
   USERNAME_FIELD = "phone_num"
   REQUIRED_FIELDS = ["password", "email", "first_name", "last_name", "user_group"]
+  class Meta(AbstractUser.Meta):
+    pass
   
-  
+# class Vehicle(models.Model):
