d7e7c2e97851b4f0441521a8b254db9245a58380
Tono-vacn <yuchen.jiang@duke.edu>
2024-01-23 17:51:21 -0500

    add bootstrap

--- web-app/base/views.py
+++ web-app/base/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+# from multiprocessing import context
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+# from .models import Question, Choice
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 # Create your views here.
 
-def index(request):
-  return HttpRequest("test, test")
+# def index(request):
+#   return HttpRequest("test")
+
+# def IndexView(generic.ListView):
+#   template_name = "base/index.html"
+#   context_object_name = "latest_question_list"
+  
