568c1be979ffd2de58f60e0f9700060d133e400f
Tono-vacn <yuchen.jiang@duke.edu>
2024-01-24 14:50:47 -0500

    fix: change model name

--- web-app/base/admin.py
+++ web-app/base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Ride, Group
+from .models import CustomUser, Ride, Group
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(CustomUser)
 admin.site.register(Ride)
 admin.site.register(Group)
--- web-app/base/views.py
+++ web-app/base/views.py
@@ -7,9 +7,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.contrib import auth,messages
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 # Create your views here.
 
+from .models import *
+from .forms import *
 
+
+
+def login(request):
+  if request.user.is_authenticated:
+    return HttpResponseRedirect(reverse("base:index"))
+  else:
+    if request.method == "POST":
+      username = request.POST.get("username")
+      password = request.POST.get("password")
+      user = auth.authenticate(username = username, password = password)    
 def index(request):
   return HttpResponse("test")
 
