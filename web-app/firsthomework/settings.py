57ed71a31364c901e23a80ee8370db32eab7bc80
junyan <junyantang1008@gmail.com>
2024-02-07 09:27:37 -0500

    Fixed docker compose

--- web-app/firsthomework/settings.py
+++ web-app/firsthomework/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': 'db',
         'PORT': 5432,
     }
